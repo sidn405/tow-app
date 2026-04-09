3e6d9b3a44178ca81f3948078b4f9ff027560753
Sidney Muhammad <info@sidneym.com>
2026-04-09 13:22:50 -0500

    Added payment_method

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -14,6 +14,7 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str = Field(min_length=8, max_length=128)
     role: UserRole
+    payment_method_id: str = None
 
 class UserLogin(BaseModel):
     email: EmailStr
